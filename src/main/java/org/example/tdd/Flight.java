77a5d8524044b4c658a0ca199747f3ad0be06a69
Mustafa Kurt <info.mkurt@gmail.com>
2020-05-16 23:28:41 +0300

    [IMP] standard test.

---
+++ src/main/java/org/example/tdd/Flight.java
@@ -0,0 +1,56 @@
+package org.example.tdd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class Flight {
+
+    private String id;
+    private List<Passenger> passengerList = new ArrayList<>();
+    private String flightType;
+
+    public Flight(String id, String flightType) {
+        this.id = id;
+        this.flightType = flightType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Passenger> getPassengerList() {
+        return Collections.unmodifiableList(passengerList);
+    }
+
+    public String getFlightType() {
+        return flightType;
+    }
+
+    public boolean addPassenger(Passenger passenger) {
+        switch (flightType) {
+            case "Economy":
+                return passengerList.add(passenger);
+            case "Business":
+                if(passenger.isVip())
+                    return passengerList.add(passenger);
+                return false;
+            default:
+                throw new RuntimeException("Unknown Type: " + flightType);
+        }
+    }
+
+    public boolean removePassenger(Passenger passenger) {
+        switch (flightType) {
+            case "Economy":
+                if(!passenger.isVip())
+                    return passengerList.remove(passenger);
+                return false;
+            case "Business":
+                return false;
+            default:
+                throw new RuntimeException("Unknown Type: " + flightType);
+        }
+    }
+
+}
